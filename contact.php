e847e918d5522172a1d959ce33cc189525dabbaf
Hollabyte <mattholladay@Matts-MacBook-Pro.local>
2016-12-10 18:36:51 -0600

    About and Contact

---
+++ contact.php
@@ -0,0 +1,18 @@
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$human = intval($_POST['human']);
+
+$from = 'Demo Contact Form'; 
+$to = 'example@domain.com'; 
+$subject = 'Message from Contact Demo ';
+ 
+$body = "From: $name\n E-Mail: $email\n Message:\n $message";
+
+if (!$_POST['name']) {
+	$errName = 'Please enter your name';
+}
+
+if (!$_POST['email'] || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+	$errEmail = 'Please enter a valid email address';
+}
